e7270e6bd1c5103d2ba6570d4c9b127381eac386
Yihang Wang <wangyihanger@gmail.com>
2022-01-27 17:12:38 +0800

    feat(*.py): support remove pattern like: a.txt a(1).txt, all duplicated file will be removed except a.txt

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-from __future__ import print_function, unicode_literals
-
-import glob
 import hashlib
 import argparse
 import os
 import inquirer
+import humanize
 
 cache = {}
-
-
-def md5(data):
-    return hashlib.md5(data.encode("utf-8")).hexdigest()
 
 
 def md5file(filename):
@@ -37,47 +31,88 @@ def choose(choices):
 
 def argparser_initialize():
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-f", "--folders", nargs="+", help="folder to scan, eg: ./Download/", required=True)
+    parser.add_argument("-f", "--folders", nargs="+", help="folder to scan, eg: ./Download/", required=True)
+    parser.add_argument("-d", "--dry-run", help="scan without actually remove files", action="store_true")
     args = parser.parse_args()
     return args
 
 
+def is_duplicate_filename_pattern(filename_0, filename_1):
+    fname_0, ext_0 = os.path.splitext(filename_0)
+    fname_1, ext_1 = os.path.splitext(filename_1)
+    '''
+    filename_0 = "foo.jpg"
+    filename_1 = "foo(1).jpg"
+    '''
+    if fname_1.startswith(fname_0):
+        print(1)
+        r = fname_1[len(fname_0):].strip()
+        if r[0] == "(" and r[-1] == ")" and (r[1:-1]).isdigit():
+            return True, filename_1
+    '''
+    filename_0 = "foo(1).jpg"
+    filename_1 = "foo.jpg"
+    '''
+    if fname_0.startswith(fname_1):
+        r = fname_0[len(fname_1):].strip()
+        if r[0] == "(" and r[-1] == ")" and (r[1:-1]).isdigit():
+            return True, filename_0
+    return False, None
+
+
+def uniquify(folder, dry_run):
+    tbd = [folder]
+    space_saved = 0
+    while len(tbd) != 0:
+        path = tbd.pop()
+        print("Scanning {}".format(path))
+        with os.scandir(path) as it:
+            # cluster via file size
+            sizetable = {}
+            for entry in it:
+                pathname = entry.path
+                if entry.is_dir() and pathname not in [".", ".."]:
+                    tbd.append(entry.path)
+                elif entry.is_file():
+                    size = os.path.getsize(pathname)
+                    if size not in sizetable.keys():
+                        sizetable[size] = []
+                    sizetable[size].append(pathname)
+            for k, v in sizetable.items():
+                if len(v) != 1:
+                    # calculate md5 of a list of files in which the size of files are the same
+                    hashtable = {}
+                    for filepath in v:
+                        h = md5file(filepath)
+                        if h not in hashtable.keys():
+                            hashtable[h] = []
+                        hashtable[h].append(filepath)
+                    for hk, hv in hashtable.items():
+                        if len(hv) > 1:
+                            print("Removing duplicated files ({}) which has md5 {}".format(humanize.naturalsize(k), hk))
+                            basepath = None
+                            for hvv in hv:
+                                if ("(" not in hvv) and (")" not in hvv):
+                                    basepath = hvv
+                            if basepath != None:
+                                print("{} will be safely stored".format(basepath.replace(folder, "")))
+                                for hvv in hv:
+                                    if hvv != basepath:
+                                        print("\tremove {}".format(hvv))
+                                        space_saved += os.path.getsize(hvv)
+                                        if not dry_run:
+                                            os.remove(hvv)
+                                        print("{} saved".format(
+                                            humanize.naturalsize(space_saved)))
+                            print("")
+    print("{} saved".format(humanize.naturalsize(space_saved)))
+
+
 def main():
     args = argparser_initialize()
-    # Bug dirty fix for: https://bugs.python.org/issue39845
     folders = args.folders
     for folder in folders:
-        if folder.endswith('"'):
-            folder = folder[:-1]
-        if not folder.endswith(os.path.sep):
-            folder += os.path.sep
-        # Scan files in folder and calculate md5 for file content
-        for filename in glob.iglob('{}**{}*'.format(folder, os.path.sep), recursive=True):
-            if os.path.isfile(filename):
-                key = md5file(filename)
-                if not key in cache.keys():
-                    cache[key] = []
-                cache[key].append(filename)
-    deleted_files = []
-    total_duplicated_file = 0
-    # Detect duplicated files
-    for _, v in cache.items():
-        if len(v) > 1:
-            # Choose files to delete
-            total_duplicated_file += len(v) - 1
-            result = choose(v)
-            files_to_be_deleted = result['files_to_be_deleted']
-            if len(files_to_be_deleted) == 0:
-                continue
-            # Confirm to delete
-            if confirm(files_to_be_deleted):
-                for filename in files_to_be_deleted:
-                    # Remove files selected
-                    os.remove(filename)
-                    deleted_files.append(filename)
-    print("{}/{} duplicate files deleted.".format(len(deleted_files),
-                                                  total_duplicated_file))
+        uniquify(folder, args.dry_run)
 
 
 if __name__ == "__main__":
